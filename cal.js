13ca8f2e5687183ac82250410a1349ebeab54f0b
inamul1621 <islaminamul1604@gmail.com>
2025-04-26 16:15:34 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,13 @@
+function appendValue(val){
+    document.getElementById("result").value += val;
+}
+function clearResult(){
+    document.getElementById("result").value = " ";
+}
+function calculate(){
+    try{
+        document.getElementById("result").value = eval(document.getElementById("result").value);
+    }catch{
+        document.getElementById("result").value = "error";
+    }
+}
